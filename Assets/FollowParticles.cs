c849bc2800b5647364cf4a2a8906a339b6f736bf
Unknown <dean3712@gmail.com>
2019-01-26 14:43:05 -0700

    First commit

---
+++ Assets/FollowParticles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class FollowParticles : MonoBehaviour
+{
+    public VisualEffect effect;
+    public Transform m_Target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = m_Target.position - transform.position;
+        effect.SetVector3("Direction", direction);
+    }
+}
